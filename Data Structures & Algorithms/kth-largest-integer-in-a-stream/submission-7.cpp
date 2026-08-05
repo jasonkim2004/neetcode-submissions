91db2fdd708c92ba4d717f5768c0d37cd3c94a65
jasonkim2004 <56300660+jasonkim2004@users.noreply.github.com>
2026-08-05 15:17:46 -0400

    Add: kth-largest-integer-in-a-stream - submission-7

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-7.cpp
@@ -0,0 +1,35 @@
+// Time complexity: 
+// O(m∗logk)
+// Space complexity:
+// O(k)
+
+class KthLargest {
+private:
+    int size = 0;
+public:
+    std::priority_queue<
+        int,
+        std::vector<int>,
+        std::greater<int>
+    > minHeap;
+
+    KthLargest(int k, vector<int>& nums) {
+        size = k;
+        for (int num : nums) {
+            minHeap.push(num);
+            while (minHeap.size() > k) {
+                minHeap.pop();
+            }
+        }
+    }
+
+    int add(int val) {
+        minHeap.push(val);
+        while (minHeap.size() > size) {
+            minHeap.pop();
+        }
+        return minHeap.top();
+    }
+};
+
+
