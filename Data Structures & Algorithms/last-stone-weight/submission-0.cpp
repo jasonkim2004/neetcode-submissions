6c6142524fb5bf0b7e801c4e4d6239369e798d48
jasonkim2004 <56300660+jasonkim2004@users.noreply.github.com>
2026-08-05 16:00:27 -0400

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        // MaxHeap with pairs
+        std::priority_queue<int> maxHeap;
+
+        for (int s : stones)
+            maxHeap.push(s);
+
+        while (maxHeap.size() > 1) {
+            int stone1 = maxHeap.top();
+            maxHeap.pop();
+            int stone2 = maxHeap.top();
+            maxHeap.pop();
+
+            maxHeap.push(abs(stone1 - stone2));
+        }
+
+        return maxHeap.top();
+    }
+};
